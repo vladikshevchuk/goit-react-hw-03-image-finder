b904580f46a2b5422be5d45730a9a9d76f439b4e
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-04-09 11:07:55 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,29 @@
-export const App = () => {
-  return (
-    <div>Привет</div>
-  );
-};
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+
+class App extends Component {
+  state = {
+    searchLine: '',
+  };
+
+  handleFormSubmit = searchLine => {
+    this.setState({ searchLine });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery searchLine={this.state.searchLine} />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  searchLine: PropTypes.string,
+}
+
+export default App;
